35d55d8f4a6cf63a0d008b3d465355127947fb1c
SAYEM088 <abusayemsarker088@gmail.com>
2023-12-04 20:59:23 +0600

    done

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('extra-btn1').addEventListener('click', function(){
+    alert("This Website is Under Development !!");
+})
+document.getElementById('extra-btn2').addEventListener('click', function(){
+    alert("This Website is Under Development !!");
+})
+document.getElementById('extra-btn3').addEventListener('click', function(){
+    alert("This Website is Under Development !!");
+})
+document.getElementById('extra-btn4').addEventListener('click', function(){
+    alert("This Website is Under Development !!");
+})
+document.getElementById('extra-btn5').addEventListener('click', function(){
+    alert("This Website is Under Development !!");
+})
