2fd3d72737826868f3be321bf4ff123a1e99a1cf
SAYEM088 <abusayemsarker088@gmail.com>
2023-12-05 21:31:48 +0600

    font size update-4

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 document.getElementById('extra-btn1').addEventListener('click', function(){
-    alert("This Website is Under Development !!");
+    alert("Opps!! This Website is Under Upgradation !!");
 })
 document.getElementById('extra-btn2').addEventListener('click', function(){
-    alert("This Website is Under Development !!");
+    alert("Opps!! This Website is Under Upgradation !!");
 })
 document.getElementById('extra-btn3').addEventListener('click', function(){
-    alert("This Website is Under Development !!");
+    alert("Opps!! This Website is Under Upgradation !!");
 })
 document.getElementById('extra-btn4').addEventListener('click', function(){
-    alert("This Website is Under Development !!");
+    alert("Opps!! This Website is Under Upgradation !!");
 })
 document.getElementById('extra-btn5').addEventListener('click', function(){
-    alert("This Website is Under Development !!");
+    alert("Opps!! This Website is Under Upgradation !!");
 })
